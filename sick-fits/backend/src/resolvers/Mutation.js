b5546a2deea9d4c61b640df90db558d6fdb5b4e9
Stacey Fenton <stacey.fenton@bglgroup.co.uk>
2019-05-15 17:39:11 +0100

    Add backend changes

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,32 @@ const Mutations = {
     }, info);
 
     return item;
+  }, 
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+
+    // remove the id from the updates
+    delete updates.id;
+
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id, title}`);
+
+    // 2. check if they own that item / have permissions
+    // TODO
+
+    // 3. delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
