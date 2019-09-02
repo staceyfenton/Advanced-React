e2f8533caf19e846815f3afc109a836f7590a649
Stacey Fenton <stacey.fenton@bglgroup.co.uk>
2019-09-02 13:46:54 +0100

    Credit card checkout

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -25,6 +25,41 @@ const Query = {
 
     // if they do, query all the users
     return ctx.db.query.users({}, info);
+  },
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in 
+    if(!ctx.request.userId) {
+      throw new Error('You are not logged in!');
+    }
+
+    // 2. Query the current order
+    const order = await ctx.db.query.order({
+      where: { id: args.id },
+    }, info);
+
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+
+    if(!ownsOrder || !!hasPermissionToSeeOrder) {
+      throw new Error('You cant see this');
+    }
+    // 4. Return the order
+    return order;
+  },
+  async orders(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+    if (!userId) {
+      throw new Error('you must be signed in!');
+    }
+    return ctx.db.query.orders(
+      {
+        where: {
+          user: { id: userId },
+        },
+      },
+      info
+    );
   }
 };
 
